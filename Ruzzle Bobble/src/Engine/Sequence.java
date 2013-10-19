57231498b01440d2a8c42f66cd36637b6844b55c
BlisterBoy <mehdi.abelha@gmail.com>
2013-10-19 18:57:48 +0200

    blabla

--- Ruzzle Bobble/src/Engine/Sequence.java
+++ Ruzzle Bobble/src/Engine/Sequence.java
@@ -2,11 +2,14 @@ package Engine;
 
 import java.util.LinkedList;
 
+import Sprite.Sprite;
+
 /**La sequence est l'objet definissant la séquence de jeu : ecran de titre, ecran de jeu etc.
  * Elle contient la liste des sprites a l'ecran et gère leur apparence en fonction du contexte
  * @author Mehdi
  *
  */
 public class Sequence {
-	LinkedList liste_sprite = new LinkedList();
+	private LinkedList liste_sprite = new LinkedList();
+	private Sprite background;
 }
