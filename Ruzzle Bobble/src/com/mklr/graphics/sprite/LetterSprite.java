044a5640b4626d1e5c21a30cb8785e9ba5c045ec
BlisterBoy <mehdi.khelifi@live.fr>
2013-11-13 12:13:08 +0100

    Truc de souris

--- Ruzzle Bobble/src/com/mklr/graphics/sprite/LetterSprite.java
+++ Ruzzle Bobble/src/com/mklr/graphics/sprite/LetterSprite.java
@@ -1,15 +1,27 @@
 package com.mklr.graphics.sprite;
 
+import com.mklr.graphics.stage.GameStage;
 import com.mklr.ruzzle.engine.Marble;
 
 public class LetterSprite extends Sprite {
 	private boolean selected;
 	private char letter;
+	private GameStage gamestage;
+	private Marble marble;
 	
-	public LetterSprite(String chemin, int x, int y, int width, int height, char letter, Marble marble){
+	public LetterSprite(String chemin, int x, int y, int width, int height, char letter, Marble marble, GameStage gamestage){
 		super(chemin, x, y, width, height);
 		this.letter = letter;
 		this.selected = false;
+		this.gamestage = gamestage;
+		this.marble = marble;
+	}
+	
+	public void onMousePressedWay(){
+		if(!selected){
+			selected = true;
+			gamestage.addToCurrentWord(letter);
+		}
 	}
 	
 	public boolean isSelected(){
