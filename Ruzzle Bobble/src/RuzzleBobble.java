d841fdbc5921fcd8c00aae9ff976b8db2f9fa529
aaylor <loic.runarvot@gmail.com>
2013-11-19 16:27:15 +0100

    multi thread on dictionary loading

--- Ruzzle Bobble/src/RuzzleBobble.java
+++ Ruzzle Bobble/src/RuzzleBobble.java
@@ -1,5 +1,6 @@
+import java.io.File;
 import java.util.HashMap;
-import java.util.Map;
+import java.util.Locale;
 
 import com.mklr.graphics.engine.Engine;
 import com.mklr.graphics.engine.Window;
@@ -9,9 +10,45 @@ import com.mklr.ruzzle.data.RuzzleDictionary;
 
 public class RuzzleBobble {
 	public static final String PATH = "";
-	
+    
+    private static Thread[] threadArray;
+
+    private static HashMap<String, RuzzleDictionary> findDictionaries() {
+        HashMap<String, RuzzleDictionary> dicList = 
+            new HashMap<String, RuzzleDictionary>();
+        File folder = new File("dict/");
+
+        if (!folder.exists()) {
+        } else if (!folder.isDirectory()) {
+        } else {
+            File[] subfiles = folder.listFiles();
+            threadArray = new Thread[subfiles.length]; 
+
+            int i = 0;
+            for (File dictFile : subfiles) {
+                String name = dictFile.getName();
+                RuzzleDictionary tmp = new RuzzleDictionary(Locale.ENGLISH, "dict/" + name);
+                
+                threadArray[i] = new Thread(tmp);
+                threadArray[i].start();
+
+                dicList.put(name, tmp);
+            }
+        }
+
+        return dicList;
+    }
+
+    private static boolean allFinished() {
+        for (Thread t : threadArray) {
+            if (t.getState() != Thread.State.TERMINATED)
+                return false;
+        }
+        return true;
+    }
+
 	public static void main(String[] args){
-		HashMap<String, RuzzleDictionary> dicList = new HashMap<String, RuzzleDictionary>();
+		HashMap<String, RuzzleDictionary> dicList = findDictionaries();
 		dicList.put("ENGLISH", new RuzzleDictionary());
 		
 		
@@ -24,5 +61,9 @@ public class RuzzleBobble {
 		
 		//Lancement de l ecran de titre
 		engine.setGameTitle();
+
+        while (allFinished());
+        System.out.println("All dictionary loaded");
+        engine.dictAreLoaded();
 	}
 }
